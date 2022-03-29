4516907bbb1031bb27d81e2bbacb805fa0273dfd
gasz <1pawsao1@gmail.com>
2022-03-29 15:11:54 +0200

    all-files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GitUsersSite from './GitUsersSite';
+
+function App() {
+    return (
+        <div className="container">
+            <GitUsersSite></GitUsersSite>
+        </div>
+    );
+}
+
+export default App;
